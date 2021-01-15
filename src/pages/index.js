7a608442e364b27d5cb987e3a9d784d61dd9739b
JasonEb <jasonebueng@gmail.com>
2021-01-14 22:25:24 -0800

    adds styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,18 @@
 import React from "react"
+import styled from "styled-components"
+import Layout from '../components/layout'
+
+const Container = styled.div`
+  outline: 2px solid black;
+  font-family: Didot,Didot LT STD,Hoefler Text,Garamond,Times New Roman,serif;
+  padding: 1em;
+`
 
 export default function Home() {
-  return <div>
-    <pre>Jason Ebueng</pre>
-    <pre>Rails / JavaScript Developer</pre>
-  </div> 
+  return <Layout>
+    <Container>
+      <div>Jason Ebueng</div>
+      <div>Rails / JavaScript Developer</div>
+    </Container>
+  </Layout> 
 }
