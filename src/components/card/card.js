940c0afdd85c65ab648e981a6b6587dac9cecfb5
JasonEb <jasonebueng@gmail.com>
2021-01-15 21:13:28 -0800

    adds cards to components

---
+++ src/components/card/card.js
@@ -0,0 +1,26 @@
+import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  outline: 2px solid black;
+  outline-style: dashed;
+  padding: 1em;
+  margin: 1em;
+  color: rgba(0,0,0,0.6);
+  background-color: #1f545d;
+      -webkit-background-clip: text;
+      -moz-background-clip: text;
+      background-clip: text;
+      color: transparent;
+      text-shadow: rgba(255,255,255,0.5) 0px 3px 3px;
+`
+
+export default function Card(props) {
+    let {font} = props
+
+  return <Container font={font} style={{fontFamily: font}}>
+        <h1>{font}</h1>
+        <h2>Lorem ipsum bisso llhasa aphso</h2>
+        <div>Developer</div>
+    </Container>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,13 @@
 import React from "react"
 import styled from "styled-components"
 import Layout from '../components/layout'
-
-const Container = styled.div`
-  outline: 2px solid black;
-  outline-style: dashed;
-  padding: 1em;
-  margin: 1em;
-  color: rgba(0,0,0,0.6);
-  background-color: #1f545d;
-      -webkit-background-clip: text;
-      -moz-background-clip: text;
-      background-clip: text;
-      color: transparent;
-      text-shadow: rgba(255,255,255,0.5) 0px 3px 3px;
-`
+import Card from '../components/card/card'
 
 export default function Home() {
   const fonts = ['Didot', 'Verdana', 'Helvetica', 'Tahoma', 'Georgia', 'Garamond', 'Courier New', 'Brush Script MT']
 
   const cards = fonts.map( (font) => {
-      return <Container font={font} style={{fontFamily: font}}>
-        <h1>{font}</h1>
-        <h2>Lorem ipsum bisso llhasa aphso</h2>
-        <div>Developer</div>
-      </Container>
+      return <Card font={font} style={{fontFamily: font}} />
     }
   )
 
