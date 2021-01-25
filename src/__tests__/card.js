973f72fe661e7918517aa6c2dffcd42e645e525f
JasonEb <jasonebueng@gmail.com>
2021-01-24 21:39:24 -0800

    adds basic card unit tests

--- src/__tests__/card.js
+++ src/__tests__/card.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { render, screen } from "@testing-library/react"
 import renderer from "react-test-renderer"
 
 import Card from "../components/card/card"
@@ -9,5 +10,34 @@ describe("Card", () => {
       .create(<Card />)
       .toJSON()
     expect(tree).toMatchSnapshot()
+  })
+
+  beforeEach( () => {
+    return render(<Card />)  
+  })
+
+  it("it should have my Name", () => {
+      const name = screen.getByText('jason ebueng')
+      expect(name).toBeTruthy()
+  })
+
+  test("it should have my Linkedin", () => {
+    const url = screen.getByText('linkedin.com/in/jasonebueng/')
+    expect(url).toBeTruthy()
+  })
+
+  test("it should have my GitHub", () => {
+    const github = screen.getByText('github.com/JasonEb')
+    expect(github).toBeTruthy()
+  })
+
+  test("it should have my email", () => {
+    const email = screen.getByText('jason.ebueng[at]gmail.com')
+    expect(email).toBeTruthy()
+  })
+
+  test("it should have my blog", () => {
+    const blog = screen.getByText('https://jasoneb.github.io/blog')
+    expect(blog).toBeTruthy()
   })
 })
