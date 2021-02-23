3730f99a0a5976ba17d9a1c42e39c2db3a29266e
JasonEb <jasonebueng@gmail.com>
2021-02-22 16:29:33 -0800

    adds portrait unit test

--- src/__tests__/card.js
+++ src/__tests__/card.js
@@ -3,6 +3,7 @@ import { render, screen } from "@testing-library/react"
 import renderer from "react-test-renderer"
 
 import LandscapeCard from "../components/card/landscapeCard"
+import PortraitCard from "../components/card/portraitCard"
 
 describe("LandScape Card", () => {
   it("renders correctly", () => {
@@ -14,6 +15,44 @@ describe("LandScape Card", () => {
 
   beforeEach( () => {
     return render(<LandscapeCard />)  
+  })
+
+  it("it should have my Name", () => {
+      const name = screen.getByText('jason ebueng')
+      expect(name).toBeTruthy()
+  })
+
+  test("it should have my Linkedin", () => {
+    const url = screen.getByText('linkedin.com/in/jasonebueng/')
+    expect(url).toBeTruthy()
+  })
+
+  test("it should have my GitHub", () => {
+    const github = screen.getByText('github.com/JasonEb')
+    expect(github).toBeTruthy()
+  })
+
+  test("it should have my email", () => {
+    const email = screen.getByText('jason.ebueng[at]gmail.com')
+    expect(email).toBeTruthy()
+  })
+
+  // test("it should have my blog", () => {
+  //   const blog = screen.getByText('https://jasoneb.github.io/blog')
+  //   expect(blog).toBeTruthy()
+  // })
+})
+
+describe("Portrait Card", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<PortraitCard />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  beforeEach( () => {
+    return render(<PortraitCard />)  
   })
 
   it("it should have my Name", () => {
